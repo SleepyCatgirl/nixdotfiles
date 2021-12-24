3c1b6daa5afc37518700ad3a55447d2ad3bfc878
kurisu <shoukaku00@gmail.com>
2021-12-24 11:47:52 +0000

    Move to unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "PC NixOS configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }: {
     nixosConfigurations.senchou-desktop = nixpkgs.lib.nixosSystem {
