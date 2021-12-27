2da9d2904a7319bd896a006ae6f63f4eb50a6e56
kurisu <shoukaku00@gmail.com>
2021-12-27 11:45:10 +0000

    nixFlakes to nixUnstable

--- nix.nix
+++ nix.nix
@@ -5,7 +5,7 @@
 	  allowUnfree = true;
   };
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixUnstable;
     extraOptions = ''
       experimental-features = nix-command flakes
       '';
