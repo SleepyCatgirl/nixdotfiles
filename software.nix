730f5527b17bd88601e4c99e4f4a076b5af8d87e
kurisu <shoukaku00@gmail.com>
2022-01-14 18:11:38 +0000

    Libvterm

--- software.nix
+++ software.nix
@@ -13,11 +13,10 @@
           R-with-pkgs = rWrapper.override{packages = with rPackages; [ggplot2 tidyverse];};
         in
           [
-            vim emacsPgtk ripgrep coreutils fd clang sqlite # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+            vim emacsPgtk ripgrep coreutils fd sqlite 
             wget git unzip # Basic tools
             vulkan-tools google-chrome discord pulsemixer # web browser, chat, audio mixer
             clojure clojure-lsp leiningen # Clojure dev
-            gcc gdb # C++ Dev
             cabal-install haskell-language-server ghc # Haskell dev
             dmenu pywal polybarFull bspwm sxhkd # WM aesthics
             gnome.gnome-tweaks # Gnome aesthics
@@ -39,12 +38,15 @@
             piper # logitech libratbagd GTK frontend
             # For configuring them
             virglrenderer # 3D Render for VMs
-            cling ccls # C++ Dev
+            cling clang ccls # C++ Dev
+            gcc gdb
+
             anki # learning
-            qutebrowser python39Packages.adblock
+            qutebrowser python39Packages.adblock # alternative web browser
             kepubify calibre # Books
             xorg.xhost
             sbcl # Common Lisp compiler
+            libvterm # Terminal emulator library for vterm
           ];
-   programs.steam.enable = true;
+   programs.steam.enable = true; # steam
 }
