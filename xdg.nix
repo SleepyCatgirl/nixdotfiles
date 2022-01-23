d7c2f6a5f2b1e6a63c8835becc1e97ee84b0b1ee
kurisu <shoukaku00@gmail.com>
2022-01-23 16:15:38 +0000

    xdg

--- xdg.nix
+++ xdg.nix
@@ -2,11 +2,10 @@
 
 {
   xdg = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
-      xdg-desktop-portal-gtk
-    ];
-    gtkUsePortal = true;
+    portal.enable = true;
+    xdg.portal.wlr.enable = true;
+    xdg.portal.gtkUsePortal = true;
+    xdg.portal.extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk ];
   };
 }
