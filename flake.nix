0a69e54b9601a9152dc0dc39971ae62a24bb12cd
kurisu <shoukaku00@gmail.com>
2022-07-02 18:28:25 +0000

    Flake

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
   description = "PC NixOS configuration";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hyprland = {
-      url = "github:vaxerski/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+##    hyprland = {
+##      url = "github:vaxerski/Hyprland";
+##      inputs.nixpkgs.follows = "nixpkgs";
+##    };
   };
-  outputs = { self, nixpkgs, hyprland }: {
+  outputs = { self, nixpkgs }: {
     nixosConfigurations.senchou-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        hyprland.nixosModules.default
-        { programs.hyprland.enable = true ;}
+#        hyprland.nixosModules.default
+#        { programs.hyprland.enable = true ;}
                 ];
     };
   };
